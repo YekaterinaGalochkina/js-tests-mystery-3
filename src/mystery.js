fedf0d6b20a72ce36a2092339582749a881f900a
Yekaterina Galochkina <galocichina.ecaterina@gmail.com>
2025-05-21 11:06:51 -0700

    implementing function

--- src/mystery.js
+++ src/mystery.js
@@ -1,1 +1,8 @@
-export const mystery3 = (string1, string2) => {};
+export const mystery3 = (string1, string2) => {
+  let counter = 0;
+  for (let i = 0; i < string1.length; i++) {
+    if (string1[i] !== string2[i]) {
+      counter++;
+    }
+  } return counter;
+};
